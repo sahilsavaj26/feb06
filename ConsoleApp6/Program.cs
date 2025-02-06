5e7472393ed356f9f8d92ebb15d5f873980ee0ba
admin <admin@SAHILSAVAJ>
2025-02-06 18:22:23 +0530

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,111 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+List<int> MyNumbers = new List<int>();
+MyNumbers.Add(11);
+MyNumbers.Add(22);
+MyNumbers.Add(33);
+MyNumbers.Add(44);
+MyNumbers.Add(33);
+MyNumbers.Add(55);
+
+
+//foreach (int item in MyNumbers)
+//{
+//    Console.WriteLine(item);
+//}
+
+List<String> Names = new List<String>();
+Names.Add("Sahil");
+Names.Add("Parth");
+Names.Add("Bhargav");
+Names.Add("Harshil");
+Names.Add("Vishal");
+
+//foreach (String item in Names)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+
+
+//Console.WriteLine(MyNumbers.Capacity); //8
+//Console.WriteLine(MyNumbers.Count); //5
+
+//Names.Remove("Bhargav");
+
+//foreach (String item in Names)
+//    {
+//        Console.WriteLine(item);
+//    }
+
+
+//List<string> Names2 = new List<string>(); //adding a name
+//Names2.Add("Jenil");
+//Names2.Add("jay");
+
+//Names.AddRange(Names2);
+
+//foreach (String Names23 in Names)
+//{
+//    Console.WriteLine(Names23);
+//}
+
+//Names.RemoveAt(3); // fourth name removed
+//foreach (String Name in Names)
+//{
+//    Console.WriteLine(Name);
+//}
+
+//Names.Insert(3,"jaydev"); // adding a name 
+//foreach (string na in Names) 
+//{
+//    Console.WriteLine(na);
+//}
+//List<String> Values = new List<String>(); // two list merge
+
+//Values.Add("Panth");
+//Values.Add("Jenu");
+//Values.Add("Priyank");
+
+//Names.InsertRange(3,Values);
+//foreach (string na in Names)
+//    {
+//        Console.WriteLine(na);
+//    }
+
+//Console.WriteLine(MyNumbers.IndexOf(33));
+//Console.WriteLine(MyNumbers.LastIndexOf(33));
+
+
+//MyNumbers.Reverse();
+
+//foreach (int numbers in MyNumbers)
+//       {
+//            Console.WriteLine(numbers);
+//        }
+
+//Names.Clear();  //clear name list
+//foreach(string name in Names)
+//{
+//    Console.WriteLine(name);
+//}
+
+//Names.Sort();     // name is add a sequence alphabetic words
+//foreach (string na in Names)
+//        {
+//            Console.WriteLine(na);
+//        }
+
+
+
+    
+
+
+Console.ReadLine();
---
+++ ConsoleApp6/employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class employee
+    {
+        public string name {  get; set; }
+        public int age { get; set; }
+        public string designation { get; set; }
+    }
+}
